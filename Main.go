d07ba85dabcd2d7a4e5eea4cde54cc016571908d
MaQuiNa1995 <maquina1995@gmail.com>
2021-10-07 22:22:49 +0200

    Creación del main

--- Main.go
+++ Main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"MaQuina1995/blockchain/model"
+	"fmt"
+)
 
+func main() {
+	chain := model.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
