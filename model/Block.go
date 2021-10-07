f97581fb09ab6d59236ed40eea4a5343e4e2db39
MaQuiNa1995 <maquina1995@gmail.com>
2021-10-07 22:10:09 +0200

    Añadidos los métodos de la blockchain y del bloque

--- model/Block.go
+++ model/Block.go
@@ -1,1 +1,48 @@
 package model
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) CalculateHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+
+	block := &Block{
+		Hash:     []byte{},
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+
+	block.CalculateHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, newBlock)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
